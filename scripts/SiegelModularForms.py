00002c4b72693a1a63d4ccd4b3e49daffd7df6f4
Eran Assaf <assaferan@gmail.com>
2023-09-14 10:12:26 -0400

    added empty spaces

--- scripts/SiegelModularForms.py
+++ scripts/SiegelModularForms.py
@@ -32,6 +32,13 @@ class SMF(SageObject):
     
     def __init__(self, k, j, prec=3, taylor_prec=20):
         self.prec = prec
+        print("Creating basis of covariants...")
+        bsc = BSC(k+j//2,j)
+        basis = bsc.GetBasis()
+        print("Done!")
+        if (len(basis) == 0):
+            self.basis = []
+            return
         if (SMF.prec < self.prec):
             print("Computing expansion of chi_6_m_2...")
             SMF.chi = get_chi6m2(4*self.prec)
@@ -43,10 +50,6 @@ class SMF(SageObject):
         q = SMF.chi.parent().base().gens()
         chi = sum([(SMF.chi.monomial_coefficient(m) + O(q[0]**prec))*m for m in SMF.chi.monomials()])
         t_chi = get_taylor_exp(chi, taylor_prec)
-        print("Creating basis of covariants...")
-        bsc = BSC(k+j//2,j)
-        basis = bsc.GetBasis()
-        print("Done!")
         print("Substituting chi_6_m_2...")
         basis_expanded = [b.subs(bsc.DCov) for b in basis]
         exps = list(chi.dict().keys())
@@ -56,23 +59,15 @@ class SMF(SageObject):
         assert len(t_chi_comps) == 7
         gens = list(reduce(lambda x,y:x.union(y), [Set(b.variables()) for b in basis]))
         x,y = t_chi.parent().gens()
-#        gens_exp = [g.subs(bsc.DCov) for g in gens]
-#        g_exps = [list(g_exp.dict().keys()) for g_exp in gens_exp]
         b_exps = list(basis_expanded[0].dict().keys())
         vals = list(basis_expanded[0].dict().values())
         U = vals[0].parent()
         a = U.gens()
-#        g_comps = [[g.dict().get(exp,U(0)) for exp in g_exps[i]] for i,g in enumerate(gens_exp)]
         b_comps = [[b.dict().get(exp,U(0)) for exp in b_exps] for b in basis_expanded]
         sub_dict = {a[i] : t_chi_comps[i] for i in range(7)}
         x,y = t_chi.parent().gens()
         qb = t_chi.parent().base().gens()
         b_comps_expanded = [[R(b_c.subs(sub_dict))+O(qb[0]**prec) for b_c in b_comps_s] for b_comps_s in b_comps]
-#        g_comps_expanded = [[R(g_c.subs(sub_dict))+O(qb[0]**prec) for g_c in g_comps_s] for g_comps_s in g_comps]
-#        g_c_e = [sum([g_comps_expanded[l][i]*x**g_exps[l][i][0] * y**g_exps[l][i][1] for i
-#                      in range(len(g_exps[l]))]) for l in range(len(g_exps))]
-#        g_sub_dict = {gens[i] : g_c_e[i] for i in range(len(gens))}
-#        b_comps_exp = [b.subs(g_sub_dict) for b in basis]
         print("Done!")
         print("Solving linear system...")
         qexps = reduce(lambda x,y: x.union(y), [reduce(lambda x,y: x.union(y), [Set(list(b_c.dict().keys())) for b_c in b_c_e]) for b_c_e in b_comps_expanded])
